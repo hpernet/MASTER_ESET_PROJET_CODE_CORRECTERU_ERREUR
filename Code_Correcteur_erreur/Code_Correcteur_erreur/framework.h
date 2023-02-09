ae109a408ffeede49631bb21ab33e107ce019300
hpernet <hugo.pernet@master-stic.fr>
2023-02-09 10:46:24 +0100

    first commit bis

---
+++ Code_Correcteur_erreur/Code_Correcteur_erreur/framework.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN             // Exclure les en-têtes Windows rarement utilisés
+// Fichiers d'en-tête Windows
+#include <windows.h>
+// Fichiers d'en-tête C RunTime
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
